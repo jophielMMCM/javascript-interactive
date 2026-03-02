0660c4d2d82952a4fdcab64b2b115094da8c0252
jophielMMCM <jtRodriguez@mcm.edu.ph>
2026-03-02 21:55:22 +0800

    Update gallery.js

--- WeekTwo/js/gallery.js
+++ WeekTwo/js/gallery.js
@@ -1,11 +1,16 @@
 function upDate(previewPic){
+    console.log("upDate success!");
+    console.log("Source: " + previewPic.src);
+    console.log("Alt text: " + previewPic.alt);
     var img = document.getElementById('image');
     img.style.backgroundImage = "url('" + previewPic.src + "')";
     img.innerHTML = previewPic.alt;
 }
 
 function unDo(){
+    console.log("unDo success!");
     var img = document.getElementById('image');
     img.style.backgroundImage = "url('')";
     img.innerHTML = "Hover over an image below to display here."
 }
+
